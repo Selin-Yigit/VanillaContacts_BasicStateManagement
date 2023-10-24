55686c70247c151fd19d883c726b11997cea540d
Selin Yigit <sln.ygt.41@gmail.com>
2023-10-24 22:07:33 +0300

    Home Page oluşturuldu.

--- lib/homePageScreen.dart
+++ lib/homePageScreen.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:vanillacontacts_course/contactBook.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey.shade600,
+        title: const Text("Vanilla Contacts"),
+        centerTitle: true,
       ),
-      body: Container(
-        color: Colors.white,
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index);
+          return ListTile(
+            title: Text(contact!.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey.shade600,
+        child: const Icon(Icons.add),
       ),
     );
   }
